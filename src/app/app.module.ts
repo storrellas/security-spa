67db67d4e5c414c596670729e3b1fa78d1adcd45
storrellas <storrellas@gmail.com>
2020-07-06 08:49:14 +0000

    ENH: Adding login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { HelloComponent } from './hello.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PasswordModule, InputTextModule, PanelModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule,    
+    BrowserAnimationsModule,
+    FormsModule,
+    PasswordModule,
+    InputTextModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
